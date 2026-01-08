be15b1f81a54a0e053b2ddfebbcd1144cc97fa65
“William <wihezuo26@aoiths.org>
2026-01-08 13:43:10 -0500

    update

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,28 @@ var profile = {
         about: "Draymond Green is a professional basketball player known for his versatility, defensive prowess, and leadership on the Golden State Warriors. He is a three-time NBA champion and has won NBA Defensive Player of the Year honors."
     },
 
-   
+    social: [
+        { facebook: "https://www.facebook.com/DraymondGreen" },
+        { X: "https://twitter.com/Money23Green" },
+        { youtube: "https://www.youtube.com/channel/UC2kJZjZ4u5j5fE5hpMogsmg" },
+        { linkedIn: "https://www.linkedin.com/in/draymond-green-662b77187/" }
+    ],
+
+    edu: {
+        name: "Michigan State University",
+        major: "Communications"
+    },
+
+    career: {
+        company: "Golden State Warriors",
+        title: "Forward",
+        role: "Defensive Specialist, Playmaker"
+    },
+
+    skills: [
+        { title: "Defense", level: 95 },
+        { title: "Playmaking", level: 90 },
+        { title: "Rebounding", level: 85 }
+    ]
+};
+
